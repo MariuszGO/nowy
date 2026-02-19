66d26d6efc4b0a70f53d3921599fd2c690bbe1dd
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-02-19 15:43:28 +0100

    n9

--- formularz1.php
+++ formularz1.php
@@ -6,21 +6,26 @@
     <title>Formularz1</title>
 </head>
 <body>
-    <form method="post">
-        <label for="imie">Imie: </label>
-        <input type="text" id="imie" name="imie"><br>
-        <label for="w">Wiek: </label>
-        <input type="number" id="w" name="wiek" min="1"><br>
-        <button>Prześlij</button>
-        
-    </form>
+
 
 
     <?php
-      $imie = @$_POST['imie'];
-      $wi = @$_POST['wiek'];
-
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $imie = $_POST['imie'];
+      $wi = $_POST['wiek'];
       echo "Imie to: $imie a wiek: $wi lat";
+    }
+    else
+        {
+        echo"<form method='post'>
+        <label for='imie'>Imie: </label>
+        <input type='text' id='imie' name='imie'><br>
+        <label for='w'>Wiek: </label>
+        <input type='number id='w' name='wiek' min='1'><br>
+        <button>Prześlij</button></form>";
+        
+    
+        }
     ?>
 
     
