75ef3bd4b3a1504547b782e42ee28cde5e58d3e2
Mariusz <m.gogola@lukasiewicz-gorlice.pl>
2026-02-19 16:22:47 +0100

    n10

--- formularz1.php
+++ formularz1.php
@@ -14,7 +14,70 @@
       $imie = $_POST['imie'];
       $wi = $_POST['wiek'];
       echo "Imie to: $imie a wiek: $wi lat";
-    }
+    
+        $duze = strtoupper($imie);
+        $duzep = mb_strtoupper($imie);
+        $male = strtolower($imie);
+
+        echo "<br><br>Zamiana na duże litery strtoupper() lub z polskimi znakami mb_strtoupper(): $duze";
+        echo "<br><br>Zamiana na duże litery strtolower() : $male";
+    
+//         | Funkcja             | Opis                                |
+// | ------------------- | ----------------------------------- |
+// | `strtoupper()`      | Zmiana na WIELKIE LITERY (ASCII)    |
+// | `strtolower()`      | Zmiana na małe litery               |
+// | `ucfirst()`         | Pierwsza litera wielka              |
+// | `lcfirst()`         | Pierwsza litera mała                |
+// | `ucwords()`         | Każde słowo wielką literą           |
+// | `mb_strtoupper()`   | Wielkie litery UTF-8 ✅              |
+// | `mb_strtolower()`   | Małe litery UTF-8                   |
+// | `mb_convert_case()` | Zaawansowana zmiana wielkości liter |
+
+
+// | Funkcja                | Opis                            |
+// | ---------------------- | ------------------------------- |
+// | `trim()`               | Usuwa spacje z początku i końca |
+// | `ltrim()`              | Usuwa z lewej                   |
+// | `rtrim()`              | Usuwa z prawej                  |
+// | `chop()`               | Alias `rtrim()`                 |
+// | `strip_tags()`         | Usuwa HTML                      |
+// | `htmlspecialchars()`   | Zamienia znaki na encje HTML    |
+// | `html_entity_decode()` | Odwraca encje HTML              |
+// | `addslashes()`         | Dodaje ukośniki                 |
+// | `stripslashes()`       | Usuwa ukośniki                  |
+
+// | Funkcja             | Opis                                          |
+// | ------------------- | --------------------------------------------- |
+// | `strpos()`          | Pozycja pierwszego wystąpienia                |
+// | `stripos()`         | Jak wyżej (bez uwzględniania wielkości liter) |
+// | `strrpos()`         | Ostatnie wystąpienie                          |
+// | `str_contains()`    | Czy zawiera tekst (PHP 8+) ✅                  |
+// | `str_starts_with()` | Czy zaczyna się od (PHP 8+)                   |
+// | `str_ends_with()`   | Czy kończy się na (PHP 8+)                    |
+// | `substr_count()`    | Liczy wystąpienia                             |
+
+// trim()
+
+// str_replace()
+
+// explode() / implode()
+
+// strlen() / mb_strlen()
+
+// strpos() / str_contains()
+
+// substr() / mb_substr()
+
+// htmlspecialchars()
+
+// preg_match()
+
+
+
+
+    
+    
+      }
     else
         {
         echo"<form method='post'>
