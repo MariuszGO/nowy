1c55fab1f18a23244143dfd1a00bac4a039b8f73
83102916617 <egzamin@poczta.pl>
2026-02-20 15:38:05 +0100

    b1

--- baza1.php
+++ baza1.php
@@ -8,11 +8,24 @@
 <body>
 
 <?php
-//Pierwsza najprostsza wersja
+//Pierwsza najprostsza wersja wysyłania
     $polaczenie = mysqli_connect("localhost","root","","szkola");
     $zapytanie ="INSERT into uczniowie values (NULL, 'Jan', 'Kowalski', 20)";
     $rezultat = mysqli_query($polaczenie,$zapytanie);
-   
+  
+//pobieranie
+    //połączenie już jest id połączenia to : $polaczenie
+    $zapytanie1 = "SELECT * FROM uczniowie";
+    $rezultat1 = mysqli_query($polaczenie,$zapytanie1);
+
+    while($tablica = mysqli_fetch_array($rezultat1)){
+        echo $tablica['imie'] . " " . $tablica['nazwisko'] . " " . $tablica['wiek'] . " <br>";
+    }
+
+
+
+
+    mysqli_close($polaczenie);
 ?>
     
 </body>
